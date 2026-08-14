e16e353496b704f2a7458912b5a1e2b357aaada0
AliAhmadiBazardeh <a.ahmadi768276@gmail.com>
2026-08-14 06:47:06 +0330

    Initial commit

---
+++ src/test/java/ir/tafsirjoo/graphapi/GraphApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.tafsirjoo.graphapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
