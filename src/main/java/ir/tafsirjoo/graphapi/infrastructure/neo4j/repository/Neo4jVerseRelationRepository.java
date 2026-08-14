78b04c40fa953498c56541a9c3bf60ab38611166
AliAhmadiBazardeh <a.ahmadi768276@gmail.com>
2026-08-14 08:44:21 +0330

    feat(infrastructure): add neo4j verse relation adapter

--- src/main/java/ir/tafsirjoo/graphapi/infrastructure/neo4j/repository/Neo4jVerseRelationRepository.java
+++ src/main/java/ir/tafsirjoo/graphapi/infrastructure/neo4j/repository/Neo4jVerseRelationRepository.java
@@ -1,4 +1,41 @@
 package ir.tafsirjoo.graphapi.infrastructure.neo4j.repository;
 
-public class Neo4jVerseRelationRepository {
+import ir.tafsirjoo.graphapi.domain.verse.model.RelationType;
+import ir.tafsirjoo.graphapi.domain.verse.model.VerseReference;
+import ir.tafsirjoo.graphapi.domain.verse.model.VerseRelation;
+import ir.tafsirjoo.graphapi.domain.verse.repository.VerseRelationRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class Neo4jVerseRelationRepository
+        implements VerseRelationRepository {
+
+    @Override
+    public VerseRelation save(VerseRelation relation) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Optional<VerseRelation> find(
+            VerseReference source,
+            VerseReference target) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<VerseRelation> findOutgoing(
+            VerseReference source
+    ) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void delete(
+            VerseReference source,
+            VerseReference target,
+            RelationType type
+    ) {
+        throw new UnsupportedOperationException();
+    }
 }
