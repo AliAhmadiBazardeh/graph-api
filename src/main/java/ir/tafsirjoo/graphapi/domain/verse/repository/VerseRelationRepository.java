ddb22fcccabbd9a7764f1f88d25e3036e1b70bef
AliAhmadiBazardeh <a.ahmadi768276@gmail.com>
2026-08-14 07:46:40 +0330

    feat(domain): define verse relation repository port

--- src/main/java/ir/tafsirjoo/graphapi/domain/verse/repository/VerseRelationRepository.java
+++ src/main/java/ir/tafsirjoo/graphapi/domain/verse/repository/VerseRelationRepository.java
@@ -1,4 +1,26 @@
 package ir.tafsirjoo.graphapi.domain.verse.repository;
 
-public class VerseRelationRepository {
+import ir.tafsirjoo.graphapi.domain.verse.model.RelationType;
+import ir.tafsirjoo.graphapi.domain.verse.model.VerseReference;
+import ir.tafsirjoo.graphapi.domain.verse.model.VerseRelation;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface VerseRelationRepository {
+
+    VerseRelation save(VerseRelation relation);
+
+    Optional<VerseRelation> find(
+            VerseReference source,
+            VerseReference target
+    );
+
+    List<VerseRelation> findOutgoing(VerseReference source);
+
+    void delete(
+            VerseReference source,
+            VerseReference target,
+            RelationType type
+    );
 }
