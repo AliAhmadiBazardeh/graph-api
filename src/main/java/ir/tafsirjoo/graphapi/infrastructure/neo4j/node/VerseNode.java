9cf3e529d8e25f2352238147a767a8f333a6078d
AliAhmadiBazardeh <a.ahmadi768276@gmail.com>
2026-08-14 08:43:44 +0330

    feat(infrastructure): add neo4j verse node

--- src/main/java/ir/tafsirjoo/graphapi/infrastructure/neo4j/node/VerseNode.java
+++ src/main/java/ir/tafsirjoo/graphapi/infrastructure/neo4j/node/VerseNode.java
@@ -1,4 +1,40 @@
 package ir.tafsirjoo.graphapi.infrastructure.neo4j.node;
 
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+
+@Node("Verse")
 public class VerseNode {
+
+    @Id
+    private String id;
+
+    private int surah;
+
+    private int number;
+
+    public VerseNode() {
+    }
+
+    public VerseNode(
+            String id,
+            int surah,
+            int number
+    ) {
+        this.id = id;
+        this.surah = surah;
+        this.number = number;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getSurah() {
+        return surah;
+    }
+
+    public int getNumber() {
+        return number;
+    }
 }
