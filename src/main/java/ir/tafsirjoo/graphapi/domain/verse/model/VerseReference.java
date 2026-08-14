014b4455c4cab36ac9a0c55637d7f7f8b589849e
AliAhmadiBazardeh <a.ahmadi768276@gmail.com>
2026-08-14 07:34:27 +0330

    feat(verse): add VerseReference domain model

--- src/main/java/ir/tafsirjoo/graphapi/domain/verse/model/VerseReference.java
+++ src/main/java/ir/tafsirjoo/graphapi/domain/verse/model/VerseReference.java
@@ -1,4 +1,22 @@
 package ir.tafsirjoo.graphapi.domain.verse.model;
 
-public class VerseReference {
+public record VerseReference(
+        int surah,
+        int verse
+) {
+
+    public VerseReference {
+        if (surah < 1 || surah > 114) {
+            throw new IllegalArgumentException("Surah must be between 1 and 114");
+        }
+
+        if (verse < 1) {
+            throw new IllegalArgumentException("Verse must be greater than 0");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return surah + ":" + verse;
+    }
 }
