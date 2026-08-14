779096aefb5d83d9e4ad9a2bd624350815dd6a6f
AliAhmadiBazardeh <a.ahmadi768276@gmail.com>
2026-08-14 07:40:58 +0330

    feat(verse): add VerseRelation domain model

--- src/main/java/ir/tafsirjoo/graphapi/domain/verse/model/VerseRelation.java
+++ src/main/java/ir/tafsirjoo/graphapi/domain/verse/model/VerseRelation.java
@@ -1,4 +1,34 @@
 package ir.tafsirjoo.graphapi.domain.verse.model;
 
+import java.util.Objects;
+
 public class VerseRelation {
+
+    private final VerseReference source;
+
+    private final VerseReference target;
+
+    private final RelationType type;
+
+    public VerseRelation(
+            VerseReference source,
+            VerseReference target,
+            RelationType type
+    ) {
+        this.source = Objects.requireNonNull(source);
+        this.target = Objects.requireNonNull(target);
+        this.type = Objects.requireNonNull(type);
+    }
+
+    public VerseReference source() {
+        return source;
+    }
+
+    public VerseReference target() {
+        return target;
+    }
+
+    public RelationType type() {
+        return type;
+    }
 }
